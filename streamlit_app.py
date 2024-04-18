c799ca896e6ee7f2ce357aac7c82adecdbcdb961
audrey dehalleux <128788531+audeha@users.noreply.github.com>
2024-04-18 11:33:54 +0200

    change main script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,29 @@
 import streamlit as st
 from st_files_connection import FilesConnection
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
+# Establish a connection using Streamlit's connection function
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("video_extract_streamlit/myfile.csv", input_format="csv", ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+# Ask the user to upload a video file
+uploaded_video = st.file_uploader("Upload a video", type=['mp4', 'mov'])
+
+# Check if a file has been uploaded
+if uploaded_video is not None:
+    # Specify the path in your bucket where the video will be stored
+    # The filename will be the same as the uploaded file's name
+    bucket_path = f"video_extract_streamlit/{uploaded_video.name}"
+
+    # Upload the video to the bucket
+    with conn.fs.open(bucket_path, 'wb') as f:
+        f.write(uploaded_video.getvalue())
+    st.success("Uploaded successfully!")
+
+# Access the underlying filesystem object and list all files
+files = conn.fs.ls('video_extract_streamlit')
+
+# Filter out video files with .mov and .mp4 extensions
+video_files = [f for f in files if f.endswith('.mov') or f.endswith('.mp4')]
+
+# Print the names of video files
+for video_file in video_files:
+    st.write(video_file)
